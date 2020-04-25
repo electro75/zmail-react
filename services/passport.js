923b7f11f60463d4a3db9a3b40eb058aaff07066
Sahil Naikdhure <sanaikdhure@gmail.com>
2020-04-25 18:23:03 +0530

    adds mongoose and Users model class

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 
 passport.use(new GoogleStrategy({
@@ -8,6 +11,8 @@ passport.use(new GoogleStrategy({
     clientSecret : keys.googleClientSecret,
     callbackURL : '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        
+        new User({
+            googleId : profile.id
+        }).save()
     })
 );
